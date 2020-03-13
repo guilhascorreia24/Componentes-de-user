8855faf711412f562060a9ecc1916f5f8768ba6b
guilhascorreia24 <55436745+guilhascorreia24@users.noreply.github.com>
2020-03-13 15:37:44 +0000

    Add files via upload

--- user/views.py
+++ user/views.py
@@ -1,17 +1,27 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
+from .models import Utilizador
 
 
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
-        if form.is_valid():
+        if form.is_valid() and Utilizador.objects.filter(email=request.POST['email'], telefone=request.POST['telefone']).exists() is False:
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
+            messages.success(request, f'Registo feito com Sucesso!')
             return redirect('blog-home')
         else:
-            return render(request,'register.html',{'form':form})
+            error=False
+            error1=False
+            error2=False
+            if Utilizador.objects.filter(email=request.POST['email'], telefone=request.POST['telefone']).exists():
+                error="Email ja existe"
+            if 1<len(request.POST['password1'])<6:
+                error1="password muito curta"
+            if request.POST['password1']!=request.POST['password2']:
+                error2="Passwords nao coincidem"
+            return render(request,'register.html',{'form':form,'error1':error,'error2':error1,'error3':error2})
     form = UserRegisterForm()
     return render(request, 'register.html',{'form':form})
